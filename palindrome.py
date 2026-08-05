94123d4da4ede869d16b1feed2a29f7e609b9085
PRAFULDAS M M <d.praful@outlook.com>
2026-08-05 17:44:39 +0530

    palindrome

---
+++ palindrome.py
@@ -0,0 +1,15 @@
+def is_palindrome(text):
+    left = 0
+    right = len(text) - 1
+
+    while left < right:
+        if text[left] != text[right]:
+            return False
+        left += 1
+        right -= 1
+
+    return True
+
+
+print(is_palindrome("madam"))   # True
+print(is_palindrome("hello"))   # False
